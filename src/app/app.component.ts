1d729db0c104db92ecf9ab0c6bbb0a3d98eff174
marceloabosch <marcelo.abosch@genmills.com>
2023-11-25 15:40:09 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,242 @@
+
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'foulClient';
+
+  private socket: WebSocket;
+  private connection: Observable<any>;
+
+  
+  private speechSynthesis = window.speechSynthesis;
+  public  record:any;
+
+  ngOnInit(): void {
+
+    this.record={
+      "MatchID": 3,
+      "MatchDate": "2023-11-04T00:00:00",
+      "ZoneID": 1,
+      "MatchStatusID": "NotStarted",
+      "MatchStatusName": "No Comenzado",
+      "ZoneName": "Norte",
+      "HomeTeamID": 2,
+      "HomeTeamName": "Galicia",
+      "AwayTeamName": "Buchardo",
+      "AwayTeamID": 1,
+      "HomeGoals": 0,
+      "AwayGoals": 0,
+      "localFileID": "a1d07072-83dc-4da5-8a24-d9487f9642a3",
+      "visitanteFileID": "b744f776-5dcf-4a61-a489-6a519908995c",
+      "HomeTeam": {
+        "TeamID": 2,
+        "TeamName": "Galicia"
+      },
+      "AwayTeam": {
+        "TeamID": 1,
+        "TeamName": "Buchardo"
+      },
+      "local": "Galicia",
+      "visitante": "Buchardo",
+      "localJugadores": [
+        {
+          "Nombre": "Lionel Messi",
+          "Camiseta": 10,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Kun Aguero",
+          "Camiseta": 9,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Diego Maradona",
+          "Camiseta": 8,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Harry Styles",
+          "Camiseta": 7,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Christian Martin",
+          "Camiseta": 6,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "El Zorro",
+          "Camiseta": 5,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Charly Garcia",
+          "Camiseta": 4,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Leon Gieco",
+          "Camiseta": 3,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Gustavo Cerati",
+          "Camiseta": 2,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Angel Carranza",
+          "Camiseta": 1,
+          "Puntos": 0,
+          "Faltas": 0
+        }
+      ],
+      "visitanteJugadores": [
+        {
+          "Nombre": "Nicolás Abosch",
+          "Camiseta": 1,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Rodrigo Diaz",
+          "Camiseta": 2,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Matias Fernandes",
+          "Camiseta": 3,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Maximiliano Tag",
+          "Camiseta": 4,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Agustín Baudot",
+          "Camiseta": 5,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Marco Diaz",
+          "Camiseta": 6,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Martín Verlatsky",
+          "Camiseta": 7,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Ignacio Bertoni",
+          "Camiseta": 8,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Nicolás Chab",
+          "Camiseta": 9,
+          "Puntos": 0,
+          "Faltas": 0
+        },
+        {
+          "Nombre": "Noel Teller",
+          "Camiseta": 10,
+          "Puntos": 0,
+          "Faltas": 0
+        }
+      ],
+      
+      "puntosLocal": 20,
+      "puntosVisitante": 9,
+      "tiempoRestante": "08:34",
+      "text": "Falta",
+      "currentMatchStatus": "Jugando"
+    };
+
+
+
+    this.socket = new WebSocket('ws://172.174.204.46:8000');
+    this.connection = this.connect()
+    this.connection.subscribe(
+      (message) => {
+        var msjJSON =  JSON.parse(message.replace(/'/g, '"'));
+        this.record= msjJSON;
+        if (this.record.text)
+        {
+         
+          var msg = new SpeechSynthesisUtterance();
+          msg.text =this.record.text;
+          speechSynthesis.speak(msg);
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+
+   
+  }
+
+  
+  public connect(): Observable<any> {
+    return new Observable(observer => {
+      this.socket.onmessage = (event) => 
+      {
+
+        var msjJSON =  JSON.parse(event.data.replace(/'/g, '"'));
+         if (msjJSON.MatchID)
+         {
+              this.record= msjJSON;
+         }   
+         else
+         {
+          this.record.tiempoRestante = msjJSON.tiempoRestante;
+          this.record.text="";
+         }
+
+        if (this.record.text)
+        {
+          
+          
+          var msg = new SpeechSynthesisUtterance();
+          msg.text =this.record.text;
+          speechSynthesis.speak(msg);
+        }
+        
+        }
+        ;
+      this.socket.onerror = (event) => observer.error(event);
+      this.socket.onclose = () => observer.complete();
+    });
+  }
+
+  public sendMessage(message: string): void {
+    this.socket.send(message);
+  }
+
+}
